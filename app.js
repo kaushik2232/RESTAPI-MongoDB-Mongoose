f8df8b56bec27bf74e731abe5f60c706db079878
kaushik.satya77@gmail.com <kaushik.satya77@gmail.com>
2019-03-19 21:47:26 -0500

    commit

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
-
+const morgan = require('morgan');
 const productRoutes = require('./API/Routes/products')
 const orderRoutes = require('./API/Routes/order')
+app.use(morgan('dev'));
 app.use('/products', productRoutes);  
 app.use('/order', orderRoutes); 
+app.use((req, res, next) => {
+    const error =  new Error('Not Found');
+    error.status(404);
+    next(error);
+});
+app.use((error, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 module.exports = app;
