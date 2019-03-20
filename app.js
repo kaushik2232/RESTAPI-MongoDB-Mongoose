21f203f94cee22339459e178ad42e2fe49d9256f
kaushik.satya77@gmail.com <kaushik.satya77@gmail.com>
2019-03-19 21:17:06 -0500

    commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const productRoutes = require('./API/Routes/products')
+const orderRoutes = require('./API/Routes/order')
 app.use('/products', productRoutes);  
-
+app.use('/order', orderRoutes); 
 module.exports = app;
