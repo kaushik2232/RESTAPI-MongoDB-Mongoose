7381a09466fc8c71293f862654f064326e8b6c68
kaushik.satya77@gmail.com <kaushik.satya77@gmail.com>
2019-03-19 21:51:59 -0500

    written error handling methods

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ app.use('/products', productRoutes);
 app.use('/order', orderRoutes); 
 app.use((req, res, next) => {
     const error =  new Error('Not Found');
-    error.status(404);
+    error.status = 404;
     next(error);
 });
 app.use((error, req, res, next) => {
-    res.status(err.status || 500);
+    res.status(error.status || 500);
     res.json({
         error: {
             message: error.message
